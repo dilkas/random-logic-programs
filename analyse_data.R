6cd265fe895de302a3add06c7252232c0b042479
Paulius Dilkas <paulius.dilkas@gmail.com>
2020-01-21 15:19:29 +0000

    Another plot

--- analyse_data.R
+++ analyse_data.R
@@ -1,23 +1,22 @@
 library(ggplot2)
 library(tikzDevice)
+library(dplyr)
+library(ggrepel)
 
 df <- read.csv("src/runtime.csv", header = FALSE, sep = ";", skip = 1)
+df <- df[, colSums(is.na(df)) < nrow(df)]
 names(df) <- c("numPredicates", "maxArity", "numVariables", "numConstants", "numAdditionalClauses", "numIndependentPairs", "maxNumNodes",
                  "solutionCount", "buildingTime", "totalTime", "initTime", "nodes", "backtracks", "fails", "restarts")
 df$percentageIndependent <- round(ifelse(df$numIndependentPairs >= 2, df$numIndependentPairs / choose(df$numPredicates, 2), 0), 2)
+df$numClauses <- df$numPredicates + df$numAdditionalClauses
 
+# Numbers
 small <- df[df$totalTime < 1, ]
 nrow(small)/nrow(df)*100
 tail(sort(df$totalTime))
+df %>% group_by(df$numPredicates) %>% tally()
 
-boxplots <- function(df, x, y) {
-  ggplot(df, aes(factor(x), y, group = x)) +
-    geom_boxplot(outlier.shape = NA) +
-    coord_trans(y = "log10", limy = c(10, quantile(y, 0.92))) +
-    scale_y_continuous(breaks = c(1, 10, 100, 1000, 10000),
-                       labels = c("1", "10", expression(10^2), expression(10^3), expression(10^4)))
-}
-
+# Phase transition
 big <- df[df$numPredicates == 8, ]
 tikz(file = "paper/phase_transition.tex", width = 6, height = 2)
 ggplot(big, aes(factor(numIndependentPairs), nodes, group = numIndependentPairs)) +
@@ -27,38 +26,100 @@ ggplot(big, aes(factor(numIndependentPairs), nodes, group = numIndependentPairs)
                      labels = c("1", "20", expression(10^2), expression(10^3), expression(10^4))) +
   stat_summary(fun.y = mean, geom = "point") +
   xlab("Independent pairs") +
-  ylab("Nodes")
+  ylab("Nodes") +
+  theme_set(theme_gray(base_size = 7))
 dev.off()
 
-# Positive correlation
-cor.test(df$numVariables, df$totalTime)
-cor.test(df$numVariables, df$nodes)
-boxplots(df, df$numVariables, df$totalTime)
-boxplots(df, df$numVariables, df$nodes)
-
-cor.test(df$numConstants, df$totalTime)
-cor.test(df$numConstants, df$nodes)
-boxplots(df, df$numConstants, df$totalTime)
-boxplots(df, df$numConstants, df$nodes)
-
-cor.test(df$numAdditionalClauses, df$totalTime)
-cor.test(df$numAdditionalClauses, df$nodes)
-boxplots(df, df$numAdditionalClauses, df$totalTime)
-boxplots(df, df$numAdditionalClauses, df$nodes)
-
-boxplots(df, df$maxNumNodes, df$totalTime)
-boxplots(df, df$maxNumNodes, df$nodes)
-
-# A phase transition in mean but not median, i.e., some instances become really hard, but most remain easy
+# Extra stuff to investigate the phase transition
+bigs <- big %>% group_by(numIndependentPairs) %>% slice(which.max(nodes))
+ggplot(bigs, aes(numIndependentPairs, nodes)) + geom_point()
+ggplot(big, aes(factor(numIndependentPairs), totalTime, group = numIndependentPairs)) +
+  geom_boxplot(outlier.shape = NA) +
+  coord_trans(y = "log10") +
+  scale_y_continuous(breaks = c(1, 20, 100, 1000, 10000),
+                     labels = c("1", "20", expression(10^2), expression(10^3), expression(10^4))) +
+  stat_summary(fun.y = mean, geom = "point") +
+  xlab("Independent pairs") +
+  ylab("Nodes")
 
 
-#               geom = "pointrange", color = "red")
-#  stat_summary(fun.y = mean, geom = "point", shape = 20, size = 4, color = "red", fill = "red")
+# Looking for correlations
 
-for (i in seq(1, 25)) {
-  print(max(big[big$numIndependentPairs == i, "nodes"]))
-  print(min(big[big$numIndependentPairs == i, "nodes"]))
+boxplots <- function(df, x, y) {
+  ggplot(df, aes(factor(x), y, group = x)) +
+    geom_boxplot(outlier.shape = NA) +
+    coord_trans(y = "log10")
 }
 
-interesting <- big[big$numIndependentPairs == 8, ]
-hist(interesting[interesting$nodes < 1000, ]$nodes)
+cor.test(df$numPredicates, df$nodes)
+boxplots(df, df$numPredicates, df$nodes)
+
+cor.test(df$maxArity, df$nodes)
+boxplots(df, df$maxArity, df$nodes)
+
+cor.test(df$numVariables, df$nodes)
+boxplots(df, df$numVariables, df$nodes)
+
+cor.test(df$numConstants, df$nodes)
+boxplots(df, df$numConstants, df$nodes)
+
+cor.test(df$numAdditionalClauses, df$nodes)
+boxplots(df, df$numAdditionalClauses, df$nodes)
+
+cor.test(df$maxNumNodes, df$nodes)
+boxplots(df, df$maxNumNodes, df$nodes)
+
+cor.test(df$numIndependentPairs, df$nodes)
+boxplots(df, df$numIndependentPairs, df$nodes)
+cor.test(df$percentageIndependent, df$nodes)
+boxplots(df, df$percentageIndependent, df$nodes)
+
+# Each parameter is a colour
+# 1, 2, 4, 8 on the x-axis
+# Something that represents the distribution of nodes on the y-axis. Start with a mean or median.
+# Pick mean and mention that the same plot for the median is similar.
+# Mention log2 on the x-axis.
+
+mytolatex <- function(name) {
+  if (name == "maxNumNodes") {
+    "$\\mathcal{M}_{\\mathcal{N}}$"
+  } else if (name == "numAdditionalClauses") {
+    "$\\mathcal{M}_{\\mathcal{C}}-|\\mathcal{P}|$"
+  } else if (name == "numPredicates") {
+    "$|\\mathcal{P}|$"
+  } else if (name == "numVariables") {
+    "$|\\mathcal{V}|$"
+  } else if (name == "numConstants") {
+    "$|\\mathcal{C}|$"
+  } else {
+    ""
+  }
+}
+
+# reformat the table to have 3 columns: parameter_name, value (1, 2, 4, 8), and mean of nodes
+effects <- data.frame(Variable = factor(), Value = integer(), mean = numeric(), aritylabel = character())
+for (Variable in c('numPredicates', 'maxArity', 'numVariables', 'numConstants', 'numAdditionalClauses', 'maxNumNodes')) {
+  foo <- df %>% group_by_at(Variable) %>% summarise(mean(nodes)) %>% rename(Value = Variable, mean = "mean(nodes)")
+  foo$Variable <- as.factor(Variable)
+  foo$aritylabel <- ""
+  effects <- rbind(effects, foo)
+}
+
+effects$label <- ifelse(effects$Value == 8, sapply(as.character(effects$Variable), mytolatex), "")
+effects$aritylabel[effects$Variable == "maxArity" & effects$Value == 4] <- "$\\mathcal{M}_{\\mathcal{A}}$"
+
+tikz(file = "paper/impact.tex", width = 3.25, height = 2.4375)
+ggplot(effects, aes(Value, mean, color = factor(Variable))) +
+  geom_line(alpha = 0.5) +
+  geom_point(aes(shape = factor(Variable), size = 1), alpha = 0.75) +
+  scale_x_continuous(trans = "log2", limits = c(1, 10)) +
+  ylab("Mean number of nodes") +
+  scale_shape_manual(values = c(15, 16, 17, 18, 19, 20)) +
+  geom_label_repel(aes(label = label, size = 1), nudge_x = 1, segment.color = "transparent", box.padding = 0.1) +
+  geom_label_repel(aes(label = aritylabel, size = 1), segment.color = "transparent", nudge_y = 300) +
+  theme_set(theme_gray(base_size = 7)) +
+  theme(legend.position = "none") +
+  xlab("Value of each parameter") +
+  scale_color_brewer(palette = "Dark2") +
+  scale_size_area(max_size = 2)
+dev.off()
